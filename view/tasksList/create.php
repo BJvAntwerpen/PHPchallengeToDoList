09f512bb49d5476ea9cf1b6d495c2c92b9dde598
Bailey van Antwerpen <99041848@mydavinci.nl>
2018-03-16 13:58:50 +0100

    update

--- view/tasksList/create.php
+++ view/tasksList/create.php
@@ -5,12 +5,12 @@
 	<label>Task duration</label><input type="text" name="taskDuration"><br>
 	<label>Task status</label>
 	<select name="taskStatus">
-		<option value="0%">incomplete</option>
+		<option value="0%">0%</option>
 		<option value="0%-25%">0%-25%</option>
 		<option value="25%-50%">25%-50%</option>
 		<option value="50%-75%">50%-75%</option>
 		<option value="75%-100%">75%-100%</option>
-		<option value="100%">completed</option>
+		<option value="100%">100%</option>
 	</select><br>
 	<input type="submit" value="Add this to the list">
 	<input type="hidden" name="ToDoListId" value="<?= $toDoID ?>">
